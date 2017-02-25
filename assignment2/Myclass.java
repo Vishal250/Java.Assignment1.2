13c20ff631c3e875381330039e1412664a324831
Vishal250 <vishalnirmal250@gmail.com>
2017-02-25 23:49:56 +0530

    Session1.A2

---
+++ assignment2/Myclass.java
@@ -0,0 +1,14 @@
+package assignment2;
+public class Myclass {
+
+public static void main(String args[])
+{
+int a=20;
+	int b= a-- - --a;
+	int c=a--;
+	int d=a>>2;
+	int e=a&b;
+System.out.println("a="+a +"\nb=" +b +"\nc=" +c +"\nd=" +d +"\ne=" +e);
+	
+}
+}
